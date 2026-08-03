01e0a6d704696077e504d1d1811178b003c8000c
CampCheck <liamy2k7@gmail.com>
2026-08-03 20:14:27 +0100

    Move trips styles into Trips.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/global.css";
+import "./styles/Layout.css";
+
 import "./styles/buttons.css";
 import "./styles/cards.css";
 import "./styles/checklist.css";
 import "./styles/dashboard.css";
 import "./styles/dialog.css";
+
+import "./styles/Trips.css";
+import "./styles/Caravan.css";
+import "./styles/Weather.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
